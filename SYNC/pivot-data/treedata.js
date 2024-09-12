5f3026b1872daaa01dbc6bede0db2015de3b81c9
Anoop-sp <anoopvallikunnam@gmail.com>
2024-09-13 03:28:30 +0530

    Create treedata.js

---
+++ SYNC/pivot-data/treedata.js
@@ -0,0 +1,398 @@
+[
+ {
+   "Risk_Capital": "APS116",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS",
+   "Division": "GLOBAL MARKETS",
+   "Product": "GLOBAL FIC",
+   "Desk": "DESK1",
+   "Sub_Desk": "SUBDESK1",
+   "Portfolio": "PORT1"
+ },
+ {
+   "Risk_Capital": "APS116",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS",
+   "Division": "GLOBAL MARKETS",
+   "Product": "GLOBAL FIC",
+   "Desk": "DESK1",
+   "Sub_Desk": "SUBDESK2",
+   "Portfolio": "PORT2"
+ },
+ {
+   "Risk_Capital": "APS116",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS",
+   "Division": "GLOBAL MARKETS",
+   "Product": "GLOBAL FIC",
+   "Desk": "DESK1",
+   "Sub_Desk": "SUBDESK2",
+   "Portfolio": "PORT3"
+ },
+ {
+   "Risk_Capital": "APS116",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS",
+   "Division": "GLOBAL MARKETS",
+   "Product": "GLOBAL FIC",
+   "Desk": "DESK1",
+   "Sub_Desk": "SUBDESK2",
+   "Portfolio": "PORT4"
+ },
+ {
+   "Risk_Capital": "APS116",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS",
+   "Division": "GLOBAL MARKETS",
+   "Product": "GLOBAL FIC1",
+   "Desk": "DESK1",
+   "Sub_Desk": "SUBDESK2",
+   "Portfolio": "PORT5"
+ },
+ {
+   "Risk_Capital": "APS116",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS",
+   "Division": "GLOBAL MARKETS",
+   "Product": "GLOBAL FIC1",
+   "Desk": "DESK1",
+   "Sub_Desk": "SUBDESK2",
+   "Portfolio": "PORT6"
+ },
+ {
+   "Risk_Capital": "APS116",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS",
+   "Division": "GLOBAL MARKETS",
+   "Product": "GLOBAL FIC1",
+   "Desk": "DESK1",
+   "Sub_Desk": "SUBDESK2",
+   "Portfolio": "PORT7"
+ },
+ {
+   "Risk_Capital": "APS116",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS",
+   "Division": "GLOBAL MARKETS1",
+   "Product": "GLOBAL FIC",
+   "Desk": "DESK2",
+   "Sub_Desk": "SUBDESK2",
+   "Portfolio": "PORT8"
+ },
+ {
+   "Risk_Capital": "APS116",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS",
+   "Division": "GLOBAL MARKETS1",
+   "Product": "GLOBAL FIC",
+   "Desk": "DESK2",
+   "Sub_Desk": "SUBDESK2",
+   "Portfolio": "PORT9"
+ },
+ {
+   "Risk_Capital": "APS116",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS",
+   "Division": "GLOBAL MARKETS1",
+   "Product": "GLOBAL FIC",
+   "Desk": "DESK2",
+   "Sub_Desk": "SUBDESK2",
+   "Portfolio": "PORT10"
+ },
+ {
+   "Risk_Capital": "APS116",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS",
+   "Division": "GLOBAL MARKETS1",
+   "Product": "GLOBAL FIC1",
+   "Desk": "DESK2",
+   "Sub_Desk": "SUBDESK2",
+   "Portfolio": "PORT11"
+ },
+ {
+   "Risk_Capital": "APS116",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS1",
+   "Division": "GLOBAL MARKETS",
+   "Product": "GLOBAL FIC",
+   "Desk": "DESK2",
+   "Sub_Desk": "SUBDESK2",
+   "Portfolio": "PORT12"
+ },
+ {
+   "Risk_Capital": "APS116",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS1",
+   "Division": "GLOBAL MARKETS",
+   "Product": "GLOBAL FIC",
+   "Desk": "DESK2",
+   "Sub_Desk": "SUBDESK2",
+   "Portfolio": "PORT13"
+ },
+ {
+   "Risk_Capital": "APS116",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS1",
+   "Division": "GLOBAL MARKETS",
+   "Product": "GLOBAL FIC",
+   "Desk": "DESK2",
+   "Sub_Desk": "SUBDESK2",
+   "Portfolio": "PORT14"
+ },
+ {
+   "Risk_Capital": "APS116",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS1",
+   "Division": "GLOBAL MARKETS",
+   "Product": "GLOBAL FIC",
+   "Desk": "DESK2",
+   "Sub_Desk": "SUBDESK2",
+   "Portfolio": "PORT15"
+ },
+ {
+   "Risk_Capital": "APS116",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS1",
+   "Division": "GLOBAL MARKETS1",
+   "Product": "GLOBAL FIC1",
+   "Desk": "DESK2",
+   "Sub_Desk": "SUBDESK2",
+   "Portfolio": "PORT16"
+ },
+ {
+   "Risk_Capital": "APS116",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS1",
+   "Division": "GLOBAL MARKETS1",
+   "Product": "GLOBAL FIC1",
+   "Desk": "DESK2",
+   "Sub_Desk": "SUBDESK3",
+   "Portfolio": "PORT17"
+ },
+ {
+   "Risk_Capital": "APS116",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS1",
+   "Division": "GLOBAL MARKETS3",
+   "Product": "GLOBAL FIC1",
+   "Desk": "DESK2",
+   "Sub_Desk": "SUBDESK3",
+   "Portfolio": "PORT18"
+ },
+ {
+   "Risk_Capital": "APS116",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS1",
+   "Division": "GLOBAL MARKETS3",
+   "Product": "GLOBAL FIC2",
+   "Desk": "DESK2",
+   "Sub_Desk": "SUBDESK3",
+   "Portfolio": "PORT19"
+ },
+ {
+   "Risk_Capital": "APS116",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS1",
+   "Division": "GLOBAL MARKETS3",
+   "Product": "GLOBAL FIC2",
+   "Desk": "DESK3",
+   "Sub_Desk": "SUBDESK3",
+   "Portfolio": "PORT20"
+ },
+ {
+   "Risk_Capital": "APS116",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS1",
+   "Division": "GLOBAL MARKETS3",
+   "Product": "GLOBAL FIC2",
+   "Desk": "DESK3",
+   "Sub_Desk": "SUBDESK3",
+   "Portfolio": "PORT21"
+ },
+ {
+   "Risk_Capital": "APS116",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS1",
+   "Division": "GLOBAL MARKETS3",
+   "Product": "GLOBAL FIC2",
+   "Desk": "DESK3",
+   "Sub_Desk": "SUBDESK3",
+   "Portfolio": "PORT22"
+ },
+ {
+   "Risk_Capital": "APS116",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS1",
+   "Division": "GLOBAL MARKETS3",
+   "Product": "GLOBAL FIC2",
+   "Desk": "DESK3",
+   "Sub_Desk": "SUBDESK3",
+   "Portfolio": "PORT23"
+ },
+ {
+   "Risk_Capital": "APS116",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS1",
+   "Division": "GLOBAL MARKETS3",
+   "Product": "GLOBAL FIC2",
+   "Desk": "DESK3",
+   "Sub_Desk": "SUBDESK3",
+   "Portfolio": "PORT24"
+ },
+ {
+   "Risk_Capital": "APS117",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS1",
+   "Division": "GLOBAL MARKETS3",
+   "Product": "GLOBAL FIC2",
+   "Desk": "DESK3",
+   "Sub_Desk": "SUBDESK3",
+   "Portfolio": "PORT25"
+ },
+ {
+   "Risk_Capital": "APS117",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS1",
+   "Division": "GLOBAL MARKETS3",
+   "Product": "GLOBAL FIC2",
+   "Desk": "DESK3",
+   "Sub_Desk": "SUBDESK3",
+   "Portfolio": "PORT26"
+ },
+ {
+   "Risk_Capital": "APS117",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS2",
+   "Division": "GLOBAL MARKETS",
+   "Product": "GLOBAL FIC",
+   "Desk": "DESK3",
+   "Sub_Desk": "SUBDESK3",
+   "Portfolio": "PORT27"
+ },
+ {
+   "Risk_Capital": "APS117",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS2",
+   "Division": "GLOBAL MARKETS",
+   "Product": "GLOBAL FIC",
+   "Desk": "DESK3",
+   "Sub_Desk": "SUBDESK3",
+   "Portfolio": "PORT28"
+ },
+ {
+   "Risk_Capital": "APS117",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS2",
+   "Division": "GLOBAL MARKETS",
+   "Product": "GLOBAL FIC1",
+   "Desk": "DESK3",
+   "Sub_Desk": "SUBDESK4",
+   "Portfolio": "PORT29"
+ },
+ {
+   "Risk_Capital": "APS117",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS2",
+   "Division": "GLOBAL MARKETS",
+   "Product": "GLOBAL FIC1",
+   "Desk": "DESK3",
+   "Sub_Desk": "SUBDESK4",
+   "Portfolio": "PORT30"
+ },
+ {
+   "Risk_Capital": "APS117",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS2",
+   "Division": "GLOBAL MARKETS1",
+   "Product": "GLOBAL FIC2",
+   "Desk": "DESK3",
+   "Sub_Desk": "SUBDESK4",
+   "Portfolio": "PORT31"
+ },
+ {
+   "Risk_Capital": "APS117",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS2",
+   "Division": "GLOBAL MARKETS1",
+   "Product": "GLOBAL FIC3",
+   "Desk": "DESK3",
+   "Sub_Desk": "SUBDESK4",
+   "Portfolio": "PORT32"
+ },
+ {
+   "Risk_Capital": "APS117",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS2",
+   "Division": "GLOBAL MARKETS3",
+   "Product": "GLOBAL FIC4",
+   "Desk": "DESK3",
+   "Sub_Desk": "SUBDESK4",
+   "Portfolio": "PORT33"
+ },
+ {
+   "Risk_Capital": "APS117",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS2",
+   "Division": "GLOBAL MARKETS3",
+   "Product": "GLOBAL FIC4",
+   "Desk": "DESK3",
+   "Sub_Desk": "SUBDESK4",
+   "Portfolio": "PORT34"
+ },
+ {
+   "Risk_Capital": "APS117",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS2",
+   "Division": "GLOBAL MARKETS3",
+   "Product": "GLOBAL FIC4",
+   "Desk": "DESK4",
+   "Sub_Desk": "SUBDESK4",
+   "Portfolio": "PORT35"
+ },
+ {
+   "Risk_Capital": "APS117",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS2",
+   "Division": "GLOBAL MARKETS3",
+   "Product": "GLOBAL FIC4",
+   "Desk": "DESK4",
+   "Sub_Desk": "SUBDESK4",
+   "Portfolio": "PORT36"
+ },
+ {
+   "Risk_Capital": "APS117",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS2",
+   "Division": "GLOBAL MARKETS3",
+   "Product": "GLOBAL FIC4",
+   "Desk": "DESK4",
+   "Sub_Desk": "SUBDESK4",
+   "Portfolio": "PORT37"
+ },
+ {
+   "Risk_Capital": "APS117",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS2",
+   "Division": "GLOBAL MARKETS3",
+   "Product": "GLOBAL FIC4",
+   "Desk": "DESK4",
+   "Sub_Desk": "SUBDESK4",
+   "Portfolio": "PORT38"
+ },
+ {
+   "Risk_Capital": "APS117",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS2",
+   "Division": "GLOBAL MARKETS3",
+   "Product": "GLOBAL FIC4",
+   "Desk": "DESK4",
+   "Sub_Desk": "SUBDESK4",
+   "Portfolio": "PORT39"
+ },
+ {
+   "Risk_Capital": "APS117",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS2",
+   "Division": "GLOBAL MARKETS3",
+   "Product": "GLOBAL FIC4",
+   "Desk": "DESK4",
+   "Sub_Desk": "SUBDESK4",
+   "Portfolio": "PORT40"
+ },
+ {
+   "Risk_Capital": "APS117",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS2",
+   "Division": "GLOBAL MARKETS3",
+   "Product": "GLOBAL FIC4",
+   "Desk": "DESK4",
+   "Sub_Desk": "SUBDESK4",
+   "Portfolio": "PORT41"
+ },
+ {
+   "Risk_Capital": "APS117",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS2",
+   "Division": "GLOBAL MARKETS4",
+   "Product": "GLOBAL FIC4",
+   "Desk": "DESK5",
+   "Sub_Desk": "SUBDESK4",
+   "Portfolio": "PORT42"
+ },
+ {
+   "Risk_Capital": "APS117",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS2",
+   "Division": "GLOBAL MARKETS4",
+   "Product": "GLOBAL FIC4",
+   "Desk": "DESK5",
+   "Sub_Desk": "SUBDESK4",
+   "Portfolio": "PORT43"
+ },
+ {
+   "Risk_Capital": "APS117",
+   "Business": "INSTITUTIONAL BANKING AND MARKETS2",
+   "Division": "GLOBAL MARKETS4",
+   "Product": "GLOBAL FIC4",
+   "Desk": "DESK5",
+   "Sub_Desk": "SUBDESK4",
+   "Portfolio": "PORT44"
+ }
+]
